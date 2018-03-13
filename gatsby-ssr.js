35c618893dbb7321d54354ef1f3091ec1a00be5f
greglobinski <greglobinski@gmail.com>
2018-03-14 00:05:04 +0100

    Remove FB SDK script tag

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -71,19 +71,5 @@ exports.onRenderBody = ({ setPostBodyComponents }) => {
         })(document);`
       }}
     />
-    // <script
-    //   key={`fb-setup`}
-    //   dangerouslySetInnerHTML={{
-    //     __html: `(function(d, s, id) {
-    //       var js, fjs = d.getElementsByTagName(s)[0];
-    //       if (d.getElementById(id)) return;
-    //       js = d.createElement(s); js.id = id;
-    //       js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=${
-    //         process.env.FB_APP_ID ? process.env.FB_APP_ID : ""
-    //       }&autoLogAppEvents=1';
-    //       fjs.parentNode.insertBefore(js, fjs);
-    //     }(document, 'script', 'facebook-jssdk'));`
-    //   }}
-    // />
   ]);
 };
